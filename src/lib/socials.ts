bb750cbec0fdffe2a5105d54e7c6409a06b5dbdd
Ekjot Singh <ekjotmakhija@gmail.com>
2025-12-23 07:45:00 +0530

    docs(src/lib): added socials.ts

---
+++ src/lib/socials.ts
@@ -0,0 +1,6 @@
+export const SOCIALS = {
+  github: "https://github.com/ekjotsinghmakhija",
+  twitter: "https://x.com/ek10sh",
+  linkedin: "https://www.linkedin.com/in/ekjot-singh-153110268/",
+  email: "mailto:ekjotmakhija@gmail.com"
+};
